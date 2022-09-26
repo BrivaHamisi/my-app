882398e0d1453a3eeb2ce7b85e66d78127ba5876
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-26 12:41:40 +0300

    Classes IN React

---
+++ src/components/MyClass.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+class MyClass extends Component {
+	
+	render(){
+		return(
+			<div>
+				<h1 classame = 'bg-primary text-white text-center'> My Email is {this.props.email}</h1>
+				<button className='btn btn-primary' onClick={this.props.myclick }>Click</button>
+			</div>
+
+			)
+	}
+}
+
+export default MyClass
---
+++ src/components/funComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FunComponent(){
+	return(
+		<h1>This is my Second function component </h1>
+
+		);
+}
+export default FunComponent;
