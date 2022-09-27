8a5855475361f58356f6c155d4f7698f31308927
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-27 23:02:16 +0300

    UseState Hooks

--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -5,6 +5,11 @@ export default function CounterHook() {
     const [count, setCount] = useState(0)
     const [text, SetText] = useState('This is a text State')
     const [info, setInfo] = useState({name:'',email:'' })
+    const [articles, setArticles] = useState(['Article One', 'Article Two', 'Article Three'])
+
+    const addArticle = () => {
+      setArticles([...articles, 'New Article'])
+    }
   return (
     <div>
         <h2>{count}</h2>
@@ -20,6 +25,17 @@ export default function CounterHook() {
 
         <h2>Name Is : {info.name} </h2>
         <h2>Email Is : {info.email} </h2>
+
+        <br/>
+        <br/>
+
+        {articles.map(article =>{
+          return <h2>{article}</h2>
+        })}
+
+        <button onClick={addArticle} className='btn btn-primary'>
+          Add Article
+        </button>
     </div>
   )
 }
